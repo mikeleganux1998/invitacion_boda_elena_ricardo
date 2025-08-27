4d720b81118da7124d51e17ca10c17121a57fd82
Miguel Rosas <111320389+mikeleganux1998@users.noreply.github.com>
2025-08-27 10:44:12 -0600

    add files

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const express = require ('express')
+const morgan = require('morgan')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+//const db = require('./conf/db')
+const app = express()
+
+//configuraciones 
+
+app.set('port',3200)
+app.set('appName','invitacion_digital')
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.set('view engine', 'pug')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(morgan('dev'))
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')))
+
+//rutas
+app.use(require('./viewEngine/routes'))
+//app.use('/api', require('./routes/_api'))
+
+//Inicializando el servidor
+app.listen(app.get('port'), () => {
+    console.log(app.get('appName'))
+    console.log("Server port:", app.get('port'))
+
+})
